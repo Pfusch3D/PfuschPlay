fa9aceaf411f36cecce41a8f87c096a23fdeef54
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-17 23:14:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import serial
 
+
+serialPort = ""
+baudrate = ""
+
+
+display = serial.Serial()
+display.baudrate = Baudrate
+display.port = serialPort
+display.open
+
+while True:
+	readData()
+
+
+def readData():
+	data = display.readline()
+	print (data)
+	return true
+
+	
