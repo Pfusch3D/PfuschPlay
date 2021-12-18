ac0acf8b301021258c3fcb7fb62a7aba8ba1e780
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-18 01:34:02 +0100

    Add files via upload
    (Development tests with bad if else constructs)

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import serial
+import requests
 
 
-serialPort = ""
-baudrate = ""
-
+serialPort = "/dev/ttyAMA0"
+baudrate = "115200"
+moonrakerURL = "http://localhost:7125/api/printer/command"
 
 display = serial.Serial()
-display.baudrate = Baudrate
+display.baudrate = baudrate
 display.port = serialPort
-display.open
-
-while True:
-	readData()
-
+display.open()
 
 def readData():
-	data = display.readline()
-	print (data)
-	return true
+	data = display.readline().rstrip().decode("utf-8")
+	print(data)
+	if data == "A21 X":
+		return "G28 X"
+	elif data == "A21 Y":
+		return "G28 Y"
+	elif data == "A21 Z":
+		return "G28 Z"
+	elif data == "A21 C":
+		return "G28"
+	else:
+		return data
+
+def writeData(input):
+	jdata = {"commands": [input]}
+	requests.post(moonrakerURL, json=jdata)
+
+while True:
+	r_data = readData()
+	writeData(r_data)
 
 	
