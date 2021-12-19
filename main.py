c56bacb88dfd7e1e6bc24b5e66b173c71570d9a6
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-19 14:58:43 +0100

    test

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ import json
 ws = websocket.WebSocket()
 ws.connect("ws://localhost/websocket")
 
-jsonTests = {
+SendGcode = {
     "jsonrpc": "2.0",
     "method": "printer.gcode.script",
     "params": {
-        "script": "G28"
+        "script": command
     },
     "id": 7466}
 
-ws.send(json.dumps(jsonTests))
+command = ""
 
 
-#def sendWS():
+def sendWS():
+    ws.send(json.dumps(SendGcode))
 
 
 def receiveWS():
-    print(ws.recv())
-    print("===========================")
+    data = ws.recv()
+    print(json.load(data))
 
 
-#def sendS():
+def sendS():
 
 
-#def receiveS():
+def receiveS():
+    data = display.readline().rstrip().decode("utf-8")
+    self.command = data
 
 
 while True:
