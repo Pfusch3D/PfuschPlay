496df94bf582b1bd427204eb16eb10f41f3ab8d4
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-19 12:06:13 +0100

    test

--- main.py
+++ main.py
@@ -12,30 +12,32 @@ display.baudrate = baudrate
 display.port = serialPort
 display.open()
 
+
 def readData():
-	data = display.readline().rstrip().decode("utf-8")
-	#print(data)
-	return data
+    data = display.readline().rstrip().decode("utf-8")
+    # print(data)
+    return data
+
 
 def writeData(input):
-	jdata = {"commands": [input]}
-	requests.post(moonrakerURL, json=jdata)
+    jdata = {"commands": [input]}
+    requests.post(moonrakerURL, json=jdata)
+
 
 def response():
     url = "http://localhost:7125/moonraker/api/response"
     headers = {'content-type': 'application/json'}
 
     payload = {
-    	"jsonrpc": "2.0",
-    	"method": "notify_gcode_response",
-    	"params": ["response message"]
+        "jsonrpc": "2.0",
+        "method": "notify_gcode_response",
+        "params": ["response message"]
     }
     res = requests.post(url, data=json.dumps(payload), headers=headers).json()
-	assert res["result"] 
+    assert res["result"] == "response message"
+
 
 while True:
-	r_data = readData()
-	writeData(r_data)
-	response()
-
-	
+    r_data = readData()
+    writeData(r_data)
+    response()
