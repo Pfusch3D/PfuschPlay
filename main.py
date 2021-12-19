5fde5ef2fcf2d6657df73ab12d92bc1361b7b8ec
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-19 11:36:38 +0100

    dev

--- main.py
+++ main.py
@@ -13,17 +13,8 @@ display.open()
 
 def readData():
 	data = display.readline().rstrip().decode("utf-8")
-	print(data)
-	if data == "A21 X":
-		return "G28 X"
-	elif data == "A21 Y":
-		return "G28 Y"
-	elif data == "A21 Z":
-		return "G28 Z"
-	elif data == "A21 C":
-		return "G28"
-	else:
-		return data
+	#print(data)
+	return data
 
 def writeData(input):
 	jdata = {"commands": [input]}
