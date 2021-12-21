700233ed2ce67618cb281f045ce11a39b25020f7
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-21 22:42:42 +0100

    test

--- second.py
+++ second.py
@@ -1,18 +1,11 @@
 import serial
 
-PfuschPlay = {
-    # /dev/ttyAMA0 = GPIO UART.
-    "serialPort": "/dev/ttyAMA0",
-    # Default baudrate. General baudrates: 9600, 115200, 250000
-    "baudrate": "115200",
-    # Only change URL when you run PfuschPlay on other Linux Machine.
-    "websocketURL": "ws://localhost/websocket"
-}
+display = serial.Serial("/dev/ttyAMA0", 115200, 1)
 
-display = serial.Serial()
-display.baudrate = PfuschPlay["baudrate"]
-display.port = PfuschPlay["serialPort"]
-display.open()
+#display.open()
+
+
+
 
 
 display.write(b"Hello World sagt das Python Programm")
