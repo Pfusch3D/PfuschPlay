351c8a3bb41ea388afad8268c969ec3d28929d07
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-23 21:13:32 +0100

    test

---
+++ ver3.py
@@ -0,0 +1,32 @@
+import websocket
+import threading
+from time import sleep
+
+
+def on_message(ws, message):
+    print message
+
+
+def on_close(ws):
+    print "### closed ###"
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        "ws://localhost/websocket", on_message=on_message, on_close=on_close)
+    ws = threading.Thread(target=ws.run_forever)
+    ws.daemon = False
+    ws.start()
+
+    conn_timeout = 5
+    while not ws.sock.connected and conn_timeout:
+        sleep(1)
+        conn_timeout -= 1
+
+    msg_counter = 0
+    while ws.sock.connected:
+        ws.send('Hello world %d' % msg_counter)
+        print("Hello World!")
+        sleep(1)
+        msg_counter += 1
