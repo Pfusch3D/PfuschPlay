54c43804e5dd7214486dbee396d2a6d8d58c5cd1
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-23 18:23:23 +0100

    test

--- ver2.py
+++ ver2.py
@@ -1,32 +1,39 @@
 import websocket
+import serial
 import _thread
 import time
 
+display = serial.Serial()
+# Load baudrate from config file:
+display.baudrate = config.PfuschPlay["baudrate"]
+# Load port from config file:
+display.port = config.PfuschPlay["serialPort"]
+# Wait for Bootloader of TFT
+time.sleep(2)
+display.open()
+
 def on_message(ws, message):
-    print(ws)
+    print(message)
 
 def on_error(ws, error):
     print(error)
 
-def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
-
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
+    # def run(*args):
+    #     for i in range(3):
+    #         time.sleep(1)
            
-        time.sleep(1)
-        #ws.close()
-        print("thread terminating...")
-    _thread.start_new_thread(run, ())
+    #     time.sleep(1)
+    #     #ws.close()
+    #     print("thread terminating...")
+    # _thread.start_new_thread(run, ())
+    print("Websocket gestartet")
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://localhost/websocket",
                               on_open=on_open,
                               on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
+                              on_error=on_error)
 
     ws.run_forever()
