28c25cdefcff61410ea07d313b680bc0d3f1a97b
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2021-12-26 22:51:53 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-PfuschPlay = {
-    # /dev/ttyAMA0 = GPIO UART.
-    "serialPort": "/dev/ttyAMA0",
-    # Default baudrate. General baudrates: 9600, 115200, 250000
-    "baudrate": "115200",
-    # Only change URL when you run PfuschPlay on other Linux Machine.
-    "websocketURL": "ws://localhost/websocket"
-}
