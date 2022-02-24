cf61fbe4a80761188e74635295050fee9d7d42c8
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-02-24 20:54:24 +0100

    python

--- PfuschPlayPython/main.py
+++ PfuschPlayPython/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import asyncio
 import configparser
 import sys
 import serial
@@ -6,14 +7,13 @@ import serial
 config = configparser.RawConfigParser()
 config.read(str(sys.argv[1]))
 
-displaytype = config['display']['displaytype']
-serialport = config['display']['serialport']
-baudrate = config['display']['baudrate']
-websocketurl = config['display']['websocketurl']
+displaytype = str(config['display']['displaytype'])
+serialport = str(config['display']['serialport'])
+baudrate = int(config['display']['baudrate'])
+websocketurl = str(config['display']['websocketurl'])
 
-def connect:
-    ser = serial.Serial(port=serialport)  
-
+def connect():
+    display = serial.Serial(port=serialport, baudrate=baudrate, )  
 
 def main():
     print("Display Typ: ", displaytype)
