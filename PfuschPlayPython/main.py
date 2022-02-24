13136bee99036b0d67d8bcfe87f57167aa9af276
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-02-24 14:54:01 +0100

    Python Update

--- PfuschPlayPython/main.py
+++ PfuschPlayPython/main.py
@@ -1,6 +1,25 @@
-def __name__():
-    print("Du weißt, dass es Haft ist!!!!!")
+#!/usr/bin/python
+import configparser
+import sys
+import serial
+
+config = configparser.RawConfigParser()
+config.read(str(sys.argv[1]))
+
+displaytype = config['display']['displaytype']
+serialport = config['display']['serialport']
+baudrate = config['display']['baudrate']
+websocketurl = config['display']['websocketurl']
+
+def connect:
+    ser = serial.Serial(port=serialport)  
 
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    print("Display Typ: ", displaytype)
+    print("Serial Port: ", serialport)
+    print("Baudrate: ", baudrate) 
+    print("websocket URL: ", websocketurl)
+
+if __name__ == "__main__":
+    main()
