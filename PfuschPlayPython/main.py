8b99c97fbe772e03e4fb00a4ee007c435bf28bdf
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-03-11 23:11:26 +0100

    Ich weiß nicht, was ich hier mache.
    Wirklich ich habe keinen Plan mehr!

--- PfuschPlayPython/main.py
+++ PfuschPlayPython/main.py
@@ -3,23 +3,42 @@ import asyncio
 import configparser
 import sys
 import serial
+import websocket
 
 config = configparser.RawConfigParser()
 config.read(str(sys.argv[1]))
 
-displaytype = str(config['display']['displaytype'])
-serialport = str(config['display']['serialport'])
-baudrate = int(config['display']['baudrate'])
-websocketurl = str(config['display']['websocketurl'])
+displaytype = str(config["display"]["displaytype"])
+serialport = str(config["display"]["serialport"])
+baudrate = int(config["display"]["baudrate"])
+websocketurl = str(config["display"]["websocketurl"])
+display = serial.Serial()
+moonraker = websocket.WebSocketApp(websocketurl, on_message=messageWebsocket)
 
-def connect():
-    display = serial.Serial(port=serialport, baudrate=baudrate, )  
+
+def connectDisplay():
+    display.baudrate = baudrate
+    display.port = serialport
+    display.open()
+
+def connectWebsocket():
+    moonraker.run_forever()
+
+def readDisplay():
+    data = display.readline().decode("utf-8").replace("\r", "").replace("\n", "")
+    return data
+
+def writeDisplay(data):
+    display.write((data + "\n").encode())
+
+def messageWebsocket(moonraker, message):
+    print(message)
 
 def main():
-    print("Display Typ: ", displaytype)
-    print("Serial Port: ", serialport)
-    print("Baudrate: ", baudrate) 
-    print("websocket URL: ", websocketurl)
+    writeDisplay("J01")
 
 if __name__ == "__main__":
+    connectDisplay()
+    connectWebsocket()
     main()
+
